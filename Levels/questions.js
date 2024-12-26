f64b1e1ee8390079d277da5bfcc8b4125a85abb6
leonar21w <leonardsd.698@gmail.com>
2024-12-26 22:55:04 +0700

    problem dashboard behaviour

--- Levels/questions.js
+++ Levels/questions.js
@@ -1,7 +1,11 @@
-export class ProblemListProperty {
-    constructor(nameProblem, problemDifficultyBar, problemDifficultyText) {
-        this.nameProblem = nameProblem;
-        this.problemDifficultyBar = problemDifficultyBar;
-        this.problemDifficultyText = problemDifficultyText;
+export class Question_Details {
+    constructor(title, id, q_chosen, q_solved, q_difficulty_text, q_diff_barNumber, q_genre) {
+        this.questionName = title;
+        this.id = id;
+        this.question_chosen = q_chosen;
+        this.question_solved = q_solved;
+        this.question_diff_text = q_difficulty_text;
+        this.question_diff_bar = q_diff_barNumber;
+        this.question_genre = q_genre;
     }
 }
